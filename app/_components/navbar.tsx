b89e7d302291367bec9dbd60602d58aa55059e3a
Jun Orville Lecena <jun.lecena@comptrolla.com>
2023-10-22 01:32:01 +0800

    updated landing page with mobile

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -3,7 +3,7 @@ import { ArrowUpRight } from "lucide-react"
 
 const Navbar = () => {
   return (
-    <div className="absolute top-0 w-full h-[56px] items-center justify-between flex px-6">
+    <div className="absolute top-0 w-full h-[56px] items-center justify-between flex px-6 md:px-12">
       <div className="flex">
         <div className=" h-[32px] px-3 flex bg-[#FF5730] rounded-full items-center justify-center">
           <span className="font-extrabold text-[#EFEDED] text-xs">Melbourne</span>
@@ -13,7 +13,7 @@ const Navbar = () => {
           <span className="font-extrabold text-[#191818] text-xs">Hub</span>
         </div>
       </div>
-      <div className="flex">
+      <div className="md:flex hidden">
         <span className="h-[32px] w-32 bg-[#EFEDED] mx-1 rounded-full items-center justify-center flex text-xs text-[#FF5730] font-bold"> +500 Coaches</span>
         <span className="h-[32px] w-20 bg-[#EFEDED] mx-1 rounded-full items-center justify-center flex text-xs text-stone-700"> Login</span>
         <span className="h-[32px] w-20 bg-[#D1B7FF] mx-1 rounded-full items-center justify-center flex text-xs text-white"> Join <ArrowUpRight className="w-4 h-4 stroke-1" /></span>
--- app/page.tsx
+++ app/page.tsx
@@ -3,79 +3,41 @@ import { Lottie } from "./_components/lotties";
 
 export default function Home() {
   return (
-    <div className="h-[calc(100vh)] pt-[56px] overflow-scroll">
+    <div className="h-[calc(100vh)] pt-[56px] overflow-scroll px-6">
       <NumberOne />
-      <div className="my-10 h-[350px] p-6">
-        <div className="grid grid-cols-5 h-full">
-
-
-          <div className="col-span-1 justify-between flex flex-col">
-            <div className="flex bg-[#191818] rounded-3xl h-full"></div>
-            <div className="flex bg-[#EDEDEF] rounded-lg h-full"></div>
-          </div>
-
-
-          <div className="col-span-2 flex flex-col px-3">
-            <div className="flex h-full bg-[url('../public/waves-v1.png')] bg-cover border-[#D1B7FF] border rounded-3xl overflow-hidden">
-            </div>
-          </div>
-          <div className="col-span-1 justify-between flex flex-col">
-            <div className="flex bg-[#EDEDEF] rounded-3xl h-full"></div>
-            <div className="flex bg-[url('../public/orange-girl.png')] bg-cover  rounded-3xl h-full"></div>
-          </div>
-          <div className="col-span-1 justify-between flex flex-col">
-            <div className="flex flex-col items-center justify-center bg-[#D1B7FF] rounded-3xl h-full">
-              <span className="text-white text-lg font-bold">Mentorships</span>
-              <span className="text-white text-lg font-bold">Coaches</span>
-            </div>
-            <div className="flex bg-[#191818] rounded-lg h-full"></div>
-          </div>
-        </div>
-      </div>
+      <NumberTwo />
     </div>
   )
 }
 
 const NumberOne = () => {
   return (
-    <div className="h-[264px] p-6 my-10">
-      <div className="grid grid-cols-5 h-full">
+    <div className="md:p-6 my-10">
+      <div className="grid grid-cols-1 md:grid-cols-10 h-full">
         <LeftPanel />
+        <SegmentCount />
         <RightPanel />
       </div>
     </div>
   )
 }
 
-const LeftPanel = () => {
+const SegmentCount = () => {
   return (
-    <div className="col-span-1 border-r justify-between flex flex-col pb-[10px]">
-      <div>
-        <LibraryBig className="text-[#D1B7FF]" />
-
-        <div className="flex flex-col my-6">
-          <div className="flex items-center">
-
-            <span className="text-sm font-medium text-[#191818] my-2">Get Started </span>
-            <MoveRight className="h-5 w-5s text-[#D1B7FF] mx-3" />
-          </div>
-          <span className="text-sm font-normals text-[#999]">How it works</span>
-          <span className="text-sm font-normal text-[#999] my-2">About us</span>
-        </div>
-
+    <div className="col-span-1 justify-center md:flex">
+      <div className="h-[56px] w-[56px] bg-[#D1B7FF] items-center justify-center flex rounded-2xl">
+        <span className="text-3xl text-white font-extrabold">01</span>
       </div>
-
-      <BlogWidget />
     </div>
   )
 }
 
 const BlogWidget = () => {
   return (
-    <div className="border flex rounded-xl items-start justify-between mr-10 p-3 bg-[#EDEDEF]">
+    <div className="flex rounded-xl items-start justify-between bg-[#EDEDEF]">
       <div>
         <div className="flex items-center justify-start">
-          <span className="w-3 h-3 bg-[#D1B7FF] mr-2" />
+          <span className="w-4 h-4 bg-[#D1B7FF] mr-2" />
           <span className="font-bold text-sm">BLOG</span>
         </div>
         <span className="text-xs">Read Latest Post</span>
@@ -88,28 +50,59 @@ const BlogWidget = () => {
   )
 }
 
+const LeftPanel = () => {
+  return (
+    <div className="col-span-2 md:min-w-[200px] bg-[#EDEDEF] p-6 rounded-3xl hidden md:flex border-r justify-between flex-col">
+      <div>
+        <LibraryBig className="text-[#D1B7FF]" />
+
+        <div className="flex flex-col my-6">
+          <div className="flex items-center">
+
+            <span className="text-sm font-medium text-[#191818] my-2">Get Started </span>
+            <MoveRight className="h-5 w-5s text-[#D1B7FF] mx-3" />
+          </div>
+          <span className="text-sm font-normal text-[#999]">How it works</span>
+        </div>
+
+      </div>
+
+      <BlogWidget />
+    </div>
+  )
+}
+
 const RightPanel = () => {
   return (
-    <div className="col-span-4 pt-8 px-24 flex flex-col">
-      <div className="flex items-center">
-        <span className="text-[3rem] font-thin text-[#191818] tracking-wider">Empowering</span>
-        <ChevronsRight className="h-16 w-16 text-[#D1B7FF] mx-3 stroke-[3px]" />
+    <div className="col-span-7 md:col-span-7 flex flex-col md:justify-start justify-center">
+      <div className="flex items-center md:mt-[-6px]">
 
-        <div className="flex items-center w-72">
-          <span className="text-xs leading-tight text-[#191818]/50">Onboarded <span className="font-bold text-[#FF5730]"> +10,000 international students</span> from <span className="font-bold text-[#FF5730]">65 countries</span> worlwide.</span>
+        <span className="md:text-[3rem] text-[2rem] md:font-thin text-[#191818] tracking-wider">Empowering</span>
+        <ChevronsRight className="md:h-16 md:w-16 h-10 w-10 text-[#D1B7FF] mx-3 stroke-[3px]" />
+
+        <div className="flex items-center w-64">
+          <div className="bg-[#EDEDEF] rounded-full px-6 h-12 items-center justify-center flex flex-col">
+            <span className="text-sm text-[#FF5730] font-bold">+10,000</span>
+            <span className="text-[0.70rem]">Onboarded</span>
+          </div>
+          <div className="bg-[#EDEDEF] rounded-full px-6 h-12 items-center justify-center md:flex hidden flex-col">
+            <span className="text-sm text-[#FF5730] font-bold">65</span>
+            <span className="text-[0.70rem]">Countries</span>
+          </div>
+          {/* <span className="text-xs leading-tight text-[#191818]/50">Onboarded <span className="font-bold text-[#FF5730]"> +10,000 international students</span> from <span className="font-bold text-[#FF5730]">65 countries</span> worlwide.</span> */}
         </div>
       </div>
 
 
-      <div className="flex items-center justify-center">
-        <span className="border-[4px] border-[#EDEDEF] mx-6 px-1 py-0 rounded-xl text-[#191818] font-thin text-md">01</span>
-        <span className="text-[3rem] font-thin text-[#191818] rounded-full">international</span>
-        <span className="text-[3rem] font-bold text-[#D1B7FF] px-3 rounded-full">Students</span>
+      <div className="flex md:items-center">
+        <span className="md:text-[3rem] text-[2rem] font-normal md:font-thin text-[#191818] rounded-full mr-6">International</span>
+        <span className="md:text-[3rem] text-[2rem] font-bold text-[#D1B7FF] md:px-3 rounded-full">Students</span>
       </div>
-      <div className="flex items-center justify-end">
 
-        <span className="text-[3rem] font-thin text-[#D1B7FF] mx-3">around</span>
-        <span className="text-[3rem] font-thin text-[#191818] mr-[-24px]"> the gl</span>
+      <div className="hidden md:flex items-center">
+
+        {/* <span className="text-[3rem] font-thin text-[#D1B7FF] mr-3"></span> */}
+        <span className="text-[3rem] font-thin text-[#191818] mr-[-24px]">around the gl</span>
         <Lottie />
 
         <span className="text-[3rem] font-thin text-[#191818] ml-[-24px] tracking-wider">be</span>
@@ -118,6 +111,42 @@ const RightPanel = () => {
           <div className="flex items-center justify-center px-6">
 
             <span className="font-normal text-sm text-background ">Join us today!</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const NumberTwo = () => {
+  return (
+    <div className="my-10 md:h-[350px] h-full flex p-6">
+      <div className="grid grid-cols-5 h-full w-full">
+
+
+        <div className="col-span-1 justify-between md:flex flex-col hidden">
+          <div className="flex bg-[#191818] rounded-3xl h-full"></div>
+          <div className="flex bg-[#EDEDEF] rounded-lg h-full"></div>
+        </div>
+
+
+        <div className="col-span-5 md:col-span-2 md:flex hidden flex-col">
+          <div className="flex h-full bg-[url('../public/waves-v1.png')] bg-cover border-[#D1B7FF] border rounded-3xl overflow-hidden">
+          </div>
+        </div>
+        <div className="col-span-5 md:col-span-1 justify-between flex flex-col">
+          <div className="md:flex hidden bg-[#EDEDEF] rounded-3xl h-full"></div>
+          <div className="flex bg-[url('../public/orange-girl.png')] bg-cover  rounded-3xl h-full"></div>
+        </div>
+        <div className="col-span-5 md:col-span-1 justify-between flex flex-col">
+          <div className="flex flex-col items-center justify-center bg-[#FF5730] rounded-3xl h-full">
+            <span className="text-white text-lg font-bold">Mentorships</span>
+            <span className="text-white text-lg font-bold">Coaches</span>
+          </div>
+          <div className="md:flex hidden bg-[#191818] rounded-lg h-full"></div>
+        </div>
+        <div className="col-span-5 md:col-span-1 flex flex-col md:hidden">
+          <div className="flex h-full bg-[url('../public/waves-v1.png')] bg-cover border-[#D1B7FF] border rounded-3xl overflow-hidden">
           </div>
         </div>
       </div>
